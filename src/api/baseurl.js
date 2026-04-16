729ff2549caf3fac1380e2ec124e0a54bdf47acf
Gulhane-Shivani <shivanigulhane2002@gmail.com>
2026-04-16 17:19:57 +0530

    baseurl chnaged

--- src/api/baseurl.js
+++ src/api/baseurl.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://safeguard-advisor-backend.onrender.com/";
 
 // Lightweight fetch-based API client (no axios dependency)
 const API = {
